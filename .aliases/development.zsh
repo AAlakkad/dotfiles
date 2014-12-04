2a7e8f9f792230230312f3974be5b9a170549893
Ammar Alakkad <am.alakkad@gmail.com>
2014-12-04 10:21:09 +0200

    Add development aliases

---
+++ .aliases/development.zsh
@@ -0,0 +1,7 @@
+# launch simple http server
+alias serve="python -m SimpleHTTPServer"
+
+# quick cd to paths
+alias www="cd /home/ammar/www"
+alias iq="cd /home/ammar/www/aliqtisadi"
+alias iqt="cd /home/ammar/www/aliqtisadi/wp-content/themes/aliqtisadi"
